348a7514ee21a91d05ab5302fdf0d967214793e0
ricparron <ricardo.parron@gmail.com>
2024-08-29 17:33:55 -0300

    Meu primeiro Commit.

---
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -0,0 +1,14 @@
+package br.com.instituto.exemplo;
+
+public class ProgramaExemplo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String nome = "Ricardo";
+		
+		System.out.println("Seu nome é : " + nome);
+		
+	}
+
+}
