d6dda51d647625d137a4bb353024777aa7eaad34
viniaquino <contato.viniaquino@outlook.com>
2024-08-29 17:33:53 -0300

    Meu primeiro commit.

---
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -0,0 +1,13 @@
+package br.com.instituto.exemplo;
+
+public class ProgramaExemplo {
+
+	public static void main(String[] args) {
+		
+		String nome = "Vinicius";
+		
+		System.out.println("Seu nome é: " + nome);
+
+	}
+
+}
