96db2cb812c13442435de7f5249cfdfe68ad44e8
gamandu <gabrel.apaps@gmail.com>
2024-08-29 17:33:46 -0300

    Meu primeiro commit.

---
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -0,0 +1,13 @@
+package br.com.instituto.exemplo;
+
+public class ProgramaExemplo {
+
+	public static void main(String[] args) {
+		
+		String nome = "Gabriel";
+		
+		System.out.println("Seu nome é : " + nome);
+		
+	}
+
+}
