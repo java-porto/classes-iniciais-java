fea00936a11b35031db74d8cf4c915c4c981065e
INSTITUTO <INSTITUTO@DESKTOP-DQQ5J0G>
2024-08-29 17:34:55 -0300

    Meu primeiro commit.

---
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -0,0 +1,16 @@
+package br.com.instituto.exemplo;
+
+public class ProgramaExemplo {
+
+	public static void main(String[] args) {
+		
+	   String nome = "João";
+		
+		System.out.println("Seu nome é :" + nome);
+	
+	
+
+	}
+
+}
+
