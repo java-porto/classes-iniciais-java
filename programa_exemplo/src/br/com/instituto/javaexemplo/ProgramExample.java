d9e2d7eb051983eb777d65c232a3b57ef0bbdaae
feliperodalv <felipe.rodrigues.alves10@gmail.com>
2024-08-29 17:33:46 -0300

    Meu primeiro commit.

---
+++ programa_exemplo/src/br/com/instituto/javaexemplo/ProgramExample.java
@@ -0,0 +1,12 @@
+package br.com.instituto.javaexemplo;
+
+public class ProgramExample {
+
+	public static void main(String[] args) {
+		
+		String nome = "Felipe";
+		
+		System.out.println("Seu nome é: " + nome);
+	}
+
+}
