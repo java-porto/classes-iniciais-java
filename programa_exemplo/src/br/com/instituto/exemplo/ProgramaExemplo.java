8b6b30745595817a04a42a791da8742b2b6de2a2
gamandu <gabrel.apaps@gmail.com>
2024-09-03 16:03:56 -0300

    Tipos primitivos...

--- programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -8,6 +8,15 @@ public class ProgramaExemplo {
 		
 		System.out.println("Seu nome é : " + nome);
 		
+		final String PESSOA_JURIDICA = "PJ";
+		
+		int nr = 0;
+		byte nr1 = 127;
+		short nr2 = 32767;
+		int nr3 = 2147483647;
+		long nr4 = 2^63-1;
+
+
 	}
 
 }
