3c19fc8dc06b47e884716b96c8b4d8c08252074f
ricparron <ricardo.parron@gmail.com>
2024-09-03 16:05:58 -0300

    Tipos Primitivos

--- programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -7,6 +7,12 @@ public class ProgramaExemplo {
 
 		String nome = "Ricardo";
 		
+		//inteiros
+		byte nr1 = 127;
+		short nr2 = 32767;
+		int nr3 = 2147483647;
+		long nr4 = 2^64-13;
+		
 		System.out.println("Seu nome é : " + nome);
 		
 	}
