f6b19a515507e780a65dfcd0d163db85b5b1dc72
gamandu <gabrel.apaps@gmail.com>
2024-09-03 17:33:19 -0300

    Bolo :D

---
+++ programa_exemplo/src/br/com/instituto/teste/Teste.java
@@ -0,0 +1,26 @@
+package br.com.instituto.teste;
+
+import br.com.instituto.confeitaria.Bolo;
+
+public class Teste {
+
+	public static void main(String[] args) {
+		
+		//Instanciando Bolo
+		Bolo bolo;
+		
+		//Construindo o objeto bolo
+		bolo = new Bolo();
+		
+		//Manipulando a classe atraves do objeto construido.
+		bolo.setNome("Gabriel");
+		
+		//Recuperando dados através do método GET
+		String nome = bolo.getNome();
+		
+		System.out.println("O nome do bolo é : " + nome);
+		
+		
+	}
+
+}
