4fb64af341ee824867bcb315eacec5657c674a52
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2024-09-03 16:42:40 -0300

    Tipos primitivos...

--- programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -5,8 +5,26 @@ public class ProgramaExemplo {
 	public static void main(String[] args) {
 		
 		String nome = "Alexandre";
+		String Nome = "Alexandre";
 		
-		System.out.println("Seu nome é : " + nome);
+		String _nome = "Alexandre";
+		
+		final String PESSOA_JURIDICA = "PJ";
+		
+		//Inteiros
+		byte  nr1  = 127;
+		short nr2 = 32767; 
+		int   nr3 = 2147483647;
+		long  nr4 = 2^63-1;
+		
+		float nr5 = 3.40292347e-3F;
+		
+		char letra = 'a';
+		char letraEmHexa = '\u0022';
+		
+		System.out.println("Letra normal: " + letra);
+
+		System.out.println("Letra em Hexa : " + letraEmHexa);
 
 	}
 
