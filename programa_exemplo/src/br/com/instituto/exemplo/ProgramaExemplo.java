31e184149cae638559524a40f19033215c8f861c
INSTITUTO <INSTITUTO@DESKTOP-DQQ5J0G>
2024-09-03 16:04:00 -0300

    Tipos primitivos

--- programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -5,7 +5,10 @@ public class ProgramaExemplo {
 	public static void main(String[] args) {
 		
 	   String nome = "João";
-		
+	   
+	   final String PESSOA_JURIDICA= "PJ";
+	   
+	  	
 		System.out.println("Seu nome é :" + nome);
 	
 	
