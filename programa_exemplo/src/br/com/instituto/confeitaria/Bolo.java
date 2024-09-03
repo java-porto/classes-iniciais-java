cd85d98013b5f11b306f4bc7261cb9ded9f79fa3
viniaquino <contato.viniaquino@outlook.com>
2024-09-03 17:32:17 -0300

    Primeira classe orientação a objeto.

---
+++ programa_exemplo/src/br/com/instituto/confeitaria/Bolo.java
@@ -0,0 +1,17 @@
+package br.com.instituto.confeitaria;
+
+public class Bolo {
+	
+	private String nome;
+	private String sabor;
+	private double preco;
+	private double peso;
+	
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
