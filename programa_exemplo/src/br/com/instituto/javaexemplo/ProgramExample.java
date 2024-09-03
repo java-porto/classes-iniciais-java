4936ef60dfb6a556658f3a0a6fe745ce8d2f387b
feliperodalv <felipe.rodrigues.alves10@gmail.com>
2024-09-03 16:07:30 -0300

    Tipos primitivos...

--- programa_exemplo/src/br/com/instituto/javaexemplo/ProgramExample.java
+++ programa_exemplo/src/br/com/instituto/javaexemplo/ProgramExample.java
@@ -5,8 +5,23 @@ public class ProgramExample {
 	public static void main(String[] args) {
 		
 		String nome = "Felipe";
+		String Nome = "Felipe";
+		
+		String _nome = "Felipe";
+		
+		final String PESSOA_JURIDICA = "PJ";
+		
+		//Inteiros
+		byte nr1 = 100;
+		short nr2 = 32767;
+		int nr3 = 2147483647;
+		long nr4 = 2^63-1;
+		
+		float nr5 = 3.40292347e-38F;
 		
 		System.out.println("Seu nome é: " + nome);
+		
+		System.out.println("Seu nome é: " + Nome);
 	}
 
 }
