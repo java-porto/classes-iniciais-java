7c99ccdbff3fb442fb039d19e9b7a5b6281ec7b8
JorgeClaudino <jorge.babarovic.claudino@gmail.com>
2024-09-03 15:22:48 -0300

    meu primeiro commit.

---
+++ exemplo1/src/br/porto/instituto/curso/AloMundo.java
@@ -0,0 +1,12 @@
+package br.porto.instituto.curso;
+
+public class AloMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("Olá Mundo");
+	}
+
+}
