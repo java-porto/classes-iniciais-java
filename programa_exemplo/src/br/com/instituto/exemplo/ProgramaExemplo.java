8be9259a06e8efc21e93a286fa35ab764ff6a9cb
INSTITUTO <jossiprimo@gmail.com>
2024-09-03 16:03:22 -0300

    meu primeiro commit

---
+++ programa_exemplo/src/br/com/instituto/exemplo/ProgramaExemplo.java
@@ -0,0 +1,13 @@
+package br.com.instituto.exemplo;
+
+public class ProgramaExemplo {
+
+	public static void main(String[] args) {
+		
+		String nome = "Jossi";
+		// TODO Auto-generated method stub
+		
+		System.out.println("Seu nome é : " + nome);
+	}
+
+}
